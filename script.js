13927dd68520b29cdaaf458525ac8c0b51409c4a
sreesakthi <sakthisreesakthi61@gmail.com>
2024-11-13 23:00:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let myH1 = document.getElementById("main-title")
+console.log(myH1)
+
+let myContainer=document.getElementsByClassName("container")
+console.log(myContainer)
+
+let mySection=document.getElementsByClassName("section")
+console.log(mySection)
+
+let myP=document.getElementsByTagName("p")
+console.log(myP)
+
+let button1=document.querySelector(".btn")
+console.log(button1)
+
+let mySection2=document.querySelector(".section")
+console.log(mySection2)
+
+let mybuttonAll=document.querySelectorAll(".btn")
+console.log(mybuttonAll)
+
+let mypAll=document.querySelectorAll("p")
+console.log(mypAll)
